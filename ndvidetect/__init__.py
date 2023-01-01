b1e3872ccf7428ed648f26a62728d36b151965f7
aferullo <f3rullo14@gmail.com>
2022-12-31 23:12:37 -0800

    buildable to package, tested locally

---
+++ ndvidetect/__init__.py
@@ -0,0 +1,2 @@
+from ndvidetect.detect import evaluate_band_vegetation
+from ndvidetect.detect import evaluate_geotiff_vegetation
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='NDVIDetection',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
+    name='ndvi-detection',
+    author='Andrew Ferullo',
+    author_email='f3rullo14@gmail.com',
     version='0.1.0',
     description='Evaluate NDVI values of a geotif.',
-    author='Andrew Ferullo',
     license='MIT',
+    packages=find_packages(where='ndvidetect'),
+    include_package_data=True,
     python_requires='>=3.6',
     install_requires=[
         'numpy>=1.23.4',
